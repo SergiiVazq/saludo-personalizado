df0bc41d43dd23f5c6046755bd89501f3eb3a860
Sergio Vazquez <sergiovazquezvarela160bc@gmail.com>
2025-10-20 10:27:24 +0200

    final

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+        System.out.print("Digite seu nome: ");
+        String nome = input.nextLine();
+        saludar(nome);
+    }
+
+    private static void saludar(String name) {
+        System.out.println("Bienvenido " + name);
     }
 }
